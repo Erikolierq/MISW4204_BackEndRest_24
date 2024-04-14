010b13bb7c49af5493e7f91cba070dffd1cdf441
Erikolierq <e.olierq@uniandes.edu.co>
2024-04-13 22:37:30 -0500

    add celery a requirements.txt y API con cola de mensajería

--- flaskr/vistas/vistas.py
+++ flaskr/vistas/vistas.py
@@ -3,11 +3,29 @@ from flask import Response, request
 from ..modelos import db, Vuelo, VueloSchema, Reserva, ReservaSchema
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
+from celery import Celery
+
+celery = Celery(__name__)
+celery.conf.broker_url = 'amqp://guest:guest@localhost:5672/'
 
 Vuelo_Schema = VueloSchema()
 Reserva_Schema = ReservaSchema()
 
-
+@celery.task
+def crear_reserva(vuelo_id, nombre_pasajero, correo_pasajero, num_pasajeros):
+    vuelo = Vuelo.query.get(vuelo_id)
+    if not vuelo:
+        return {"error": "El vuelo no existe"}
+    if vuelo.sillas_disponibles < num_pasajeros:
+        return {"error": "No hay suficientes sillas en el vuelo"}
+    nueva_reserva = Reserva(vuelo_id=vuelo_id,
+                            nombre_pasajero=nombre_pasajero,
+                            correo_pasajero=correo_pasajero,
+                            num_pasajeros=num_pasajeros)
+    vuelo.sillas_disponibles -= num_pasajeros
+    db.session.add(nueva_reserva)
+    db.session.commit()
+    return Reserva_Schema.dump(nueva_reserva)
 
 class VistaVuelos(Resource):
 
@@ -48,21 +66,10 @@ class VistaReservas(Resource):
     def post(self):
         reserva_data = request.json
         if not all(key in reserva_data for key in ['vuelo_id', 'nombre_pasajero', 'correo_pasajero', 'num_pasajeros']):
-            response = Response(response=json.dumps({"error": "Faltan campos requeridos"}), status=400, mimetype='application/json')
-            return response
-        vuelo = Vuelo.query.get(reserva_data['vuelo_id'])
-        if not vuelo:
-            response = Response(response=json.dumps({"error": "El vuelo no existe"}), status=404, mimetype='application/json')
-            return response
-        if vuelo.sillas_disponibles < reserva_data['num_pasajeros']:
-            response = Response(response=json.dumps({"error": "No hay suficientes sillas en el vuelo"}), status=400, mimetype='application/json')
-            return response
-        nueva_reserva = Reserva(vuelo_id=reserva_data['vuelo_id'],
-                            nombre_pasajero=reserva_data['nombre_pasajero'],
-                            correo_pasajero=reserva_data['correo_pasajero'],
-                            num_pasajeros=reserva_data['num_pasajeros'])
-        vuelo.sillas_disponibles -= reserva_data['num_pasajeros']
-        db.session.add(nueva_reserva)
-        db.session.commit()
-        response = Response(response=json.dumps(Reserva_Schema.dump(nueva_reserva)), status=201, mimetype='application/json')
-        return response
+            return Response(response=json.dumps({"error": "Faltan campos requeridos"}), status=400, mimetype='application/json')
+    
+        # Envía la tarea Celery para manejar la creación de la reserva
+        task = crear_reserva.delay(reserva_data['vuelo_id'], reserva_data['nombre_pasajero'], reserva_data['correo_pasajero'], reserva_data['num_pasajeros'])
+    
+        # Puedes devolver la información de la tarea, si lo deseas
+        return Response(response=json.dumps({"task_id": task.id}), status=202, mimetype='application/json')
